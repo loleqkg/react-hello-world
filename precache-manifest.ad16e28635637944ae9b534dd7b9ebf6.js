76a95362ed3f907e64f41d58ad940f8d5990c6ee
Karol Głasek <karolglasek@gmail.com>
2020-04-30 19:16:12 +0200

    Updates

---
+++ precache-manifest.ad16e28635637944ae9b534dd7b9ebf6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "912592cfb735defc35a573133511b433",
+    "url": "/react-hello-world/index.html"
+  },
+  {
+    "revision": "84fd86c8fce82a256542",
+    "url": "/react-hello-world/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "652a599d1a99efccd345",
+    "url": "/react-hello-world/static/js/2.a430f49c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hello-world/static/js/2.a430f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84fd86c8fce82a256542",
+    "url": "/react-hello-world/static/js/main.ee0b472b.chunk.js"
+  },
+  {
+    "revision": "3bab0ec75daaa65018e8",
+    "url": "/react-hello-world/static/js/runtime-main.c38031cf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-hello-world/static/media/logo.5d5d9eef.svg"
+  }
+]);
